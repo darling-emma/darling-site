2e04c5c3673fe1f3c1044edee01ada43ed57d9e8
darling-emma <emma@darling.nyc>
2025-04-07 11:31:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 console.log("Script is connected!");
 
+<script>
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".video-fullwidth u-container-vert", {
-  scrollTrigger: ".video-fullwidth u-container-vert",
-  x: 100,
-  duration: 1,
+ScrollTrigger.defaults({
+  markers: true
 });
+</script>
+
+<script>
+// Animate From
+$(".header u-container-horiz").each(function (index) {
+  let triggerElement = $(this);
+  let targetElement = $(".wrap-50 align-left");
+
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      // trigger element - viewport
+      start: "top center",
+      end: "bottom top",
+      scrub: 1
+    }
+  });
+  tl.from(targetElement, {
+    height: "100%",
+    duration: 1
+  });
+});
+</script>
