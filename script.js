cae28eeffa6edb0e7b0e6123dbceb115d9621620
darling-emma <emma@darling.nyc>
2025-04-08 08:52:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,9 @@
 console.log("Script is connected!");
 
-<script>
 gsap.registerPlugin(ScrollTrigger);
 
-ScrollTrigger.defaults({
-  markers: true
+gsap.to(".your-element", {
+  scrollTrigger: ".your-element",
+  x: 100,
+  duration: 1,
 });
-</script>
-
-<script>
-// Animate From
-$(".textalign-center").each(function (index) {
-  let triggerElement = $(this);
-  let targetElement = $(this);
-
-  let tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: triggerElement,
-      // trigger element - viewport
-      start: "top center",
-      end: "bottom top",
-      scrub: true
-    }
-  });
-  tl.from(targetElement, {
-    opacity: 100,
-    duration: 1
-  });
-});
-</script>
