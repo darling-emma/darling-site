35ccad8bed1e4a7d8fbfedf628679b4d35c24bce
darling-emma <emma@darling.nyc>
2025-08-12 14:20:22 -0400

    fixed scrollSmoother creation

--- perpage.js
+++ perpage.js
@@ -1,59 +1,52 @@
-console.log("connected - per page - v4");
+console.log("connected - per page - v4.5");
 
 document.addEventListener("DOMContentLoaded", () => {
   gsap.registerPlugin(ScrollSmoother, SplitText)
 
-  const matchMedia = gsap.matchMedia();
+  ScrollSmoother.create({
+    wrapper: "#smooth-wrapper",
+    content: "#smooth-content",
+    smooth: 1,
+    ignoreMobileResize: true,
+    normalizeScroll: true
+  });
+
+  // Menu animation
+  const menuTrigger = document.querySelector(".mobile-links");
+  const menu = document.querySelector(".mobile-menu");
+  const bottomlinks = document.querySelector(".mobile-menu-bottom");
+
+  gsap.set(menu, { yPercent: -100, display: "none" });
+
+  menuTrigger.dataset.open = "false";
+
+  menuTrigger.addEventListener("click", () => {
+    const isClosed = menuTrigger.dataset.open === "false";
     
+    if (isClosed) {
+      let menuOpen = gsap.timeline();
 
-  // Initialize ScrollSmoother, Desktop only
-    matchMedia.add("(min-width: 768px)", () => {
-        ScrollSmoother.create({
-            wrapper: "#smooth-wrapper",
-            content: "#smooth-content",
-            smooth: 1,
-            effects: true,
-            ignoreMobileResize: true,
-            normalizeScroll: true
-        });
-    });
+      menuOpen
+      .set(menu, { display: "flex" })
+      .to(menuTrigger, { rotate: 45 })
+      .to(menu, { yPercent: 0 }, "<")
+      .from(".mobile-link", { opacity: 0, stagger: 0.1, duration: 0.3 })
+      .from(bottomlinks, { opacity: 0, duration: 0.3 }, "<");
 
-    // Menu animation
-    const menuTrigger = document.querySelector(".mobile-links");
-    const menu = document.querySelector(".mobile-menu");
-    const bottomlinks = document.querySelector(".mobile-menu-bottom");
+      menuTrigger.dataset.open = "true";
+    } else {
+      let menuClose = gsap.timeline();
 
-    gsap.set(menu, { yPercent: -100, display: "none" });
+      menuClose
+      .to(bottomlinks, { opacity: 0, duration: 0.3 })
+      .to(".mobile-link", { opacity: 0, stagger: -0.1, duration: 0.3 }, "<")
+      .to(menuTrigger, { rotate: 0 })
+      .to(menu, { yPercent: -100 }, "<")
+      .set(menu, { display: "flex" })
+      .set(".mobile-link", { opacity: 1 }, "<")
+      .set(bottomlinks, { opacity: 1 }, "<");
 
-    menuTrigger.dataset.open = "false";
-
-    menuTrigger.addEventListener("click", () => {
-      const isClosed = menuTrigger.dataset.open === "false";
-      
-      if (isClosed) {
-        let menuOpen = gsap.timeline();
-
-        menuOpen
-        .set(menu, { display: "flex" })
-        .to(menuTrigger, { rotate: 45 })
-        .to(menu, { yPercent: 0 }, "<")
-        .from(".mobile-link", { opacity: 0, stagger: 0.1, duration: 0.3 })
-        .from(bottomlinks, { opacity: 0, duration: 0.3 }, "<");
-
-        menuTrigger.dataset.open = "true";
-      } else {
-        let menuClose = gsap.timeline();
-
-        menuClose
-        .to(bottomlinks, { opacity: 0, duration: 0.3 })
-        .to(".mobile-link", { opacity: 0, stagger: -0.1, duration: 0.3 }, "<")
-        .to(menuTrigger, { rotate: 0 })
-        .to(menu, { yPercent: -100 }, "<")
-        .set(menu, { display: "flex" })
-        .set(".mobile-link", { opacity: 1 }, "<")
-        .set(bottomlinks, { opacity: 1 }, "<");
-
-        menuTrigger.dataset.open = "false";
-      }
-    });
+      menuTrigger.dataset.open = "false";
+    }
+  });
 });
