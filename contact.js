0c287e33c9407e45f57076979a9c4a47fdbd5c73
darling-emma <emma@darling.nyc>
2025-08-12 14:29:00 -0400

    Create contact.js

---
+++ contact.js
@@ -0,0 +1,34 @@
+console.log("contact - connected - v1");
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.registerPlugin(SplitText)
+
+    // AUTOALPHA
+    gsap.to(".contact-content", { autoAlpha: 1, duration: 0.2 });
+
+    // LOAD ANIMATION
+    SplitText.create(".contact-text", {
+        type: "lines",
+        autoSplit: true,
+        onSplit(self) {
+            gsap.from(self.lines, {
+                y: -5,
+                opacity: 0,
+                stagger: {
+                    amount: 0.3
+                },
+                ease: "power2.out",
+                delay: 0.2,
+            });
+        }
+    });
+
+    gsap.from(".contact-item", {
+        opacity: 0,
+        stagger: {
+            amount: 0.3
+        },
+        ease: "power2.out",
+        delay: 0.2,
+    });
+});
