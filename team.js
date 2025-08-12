c628212c1022dd162083bfafeb328ea35d9ee4f5
darling-emma <emma@darling.nyc>
2025-08-12 16:19:44 -0400

    overhaul - works on all platforms & browsers

--- team.js
+++ team.js
@@ -1,11 +1,121 @@
-console.log("connected - team - v1");
+console.log("connected - team - v2");
 
 $(document).ready(function() {
-    gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText)
+    gsap.registerPlugin(SplitText, ScrollTrigger)
 
-    const matchM = gsap.matchMedia();
+    const mm = gsap.matchMedia();
 
-    // ID + DATA-ID
+    // AUTOALPHA
+    gsap.to(".team-hero", { autoAlpha: 1, duration: 0.2 });
+    gsap.to(".image-trio", { autoAlpha: 1, duration: 1, ease: "power2.out", });
+
+    // LOAD ANIMATION
+    window.addEventListener("load", () => {
+        document.fonts.ready.then(() => {
+            SplitText.create(".brochure-para-large", {
+                type: "lines",
+                autoSplit: true,
+                onSplit(self) {
+                    return gsap.from(self.lines, {
+                        y: -5,
+                        opacity: 0,
+                        stagger: {
+                            amount: 1
+                        },
+                        ease: "power2.out",
+                        delay: 0.2,
+                    });
+                }
+            });
+        });
+    });
+
+    gsap.from(".team-image-item", {
+        opacity: 0,
+        stagger: {
+            amount: 1
+        },
+        ease: "power2.out",
+        delay: 0.2,
+    });
+
+    // TRIO ANIMATION
+    mm.add({
+
+        isMobile: "(max-width: 478px)",
+        isDesktop: "(min-width: 479px)",
+
+    }, (context) => {
+
+        let { isMobile, isDesktop } = context.conditions;
+
+        gsap.to(".team-image-wrapper", {
+            scrollTrigger: {
+                trigger: ".image-trio",
+                start: "top top",
+                end: "+=1000",
+                pin: ".image-trio",
+                scrub: true,
+                ease: "none",
+            },
+            height: isDesktop ? 0 : "auto",
+            width: isMobile ? 0 : "auto",
+            stagger: 0.1,
+        });
+
+        gsap.from(".team-trio-text-wrapper", {
+            scrollTrigger: {
+                trigger: ".image-trio",
+                start: "top top",
+                end: "+=1000",
+                scrub: true,
+                ease: "none",
+            },
+            yPercent: isDesktop ? 10 : 0,
+            xPercent: isMobile ? 10 : 0,
+            stagger: 0.1,
+            delay: 0.3,
+        });
+    });
+
+    // XL TEXT HORIZONTAL SCROLL
+    let endValue, travelWidth;
+
+    function calculateXLText() {
+        const textWidth = document.querySelector(".text-xl-wrapper").getBoundingClientRect().width;
+
+        const textContainer = document.querySelector(".text-xl");
+        const buffer = parseFloat(getComputedStyle(textContainer).paddingLeft);
+
+        const textTotal = textWidth + (buffer * 2);
+
+        travelWidth = window.innerWidth - textTotal;
+
+        endValue = travelWidth * -1.5;
+    }
+
+    window.addEventListener("load", () => {
+        document.fonts.ready.then(() => {
+            calculateXLText();
+
+            ScrollTrigger.addEventListener("refreshInit", () => calculateXLText());
+
+            gsap.to(".text-xl-wrapper", {
+                x: () => travelWidth,
+                scrollTrigger: {
+                    trigger: ".text-xl",
+                    start: "top top",
+                    end: () => `+=${endValue}`,
+                    scrub: true,
+                    pin: true,
+                    ease: "none",
+                }
+            });
+        });
+    });
+
+    // TEAM ANIMATION
+    // Id + Data-Id
     $('.team-col-item').each(function() {
         const idText = $(this).children('.idtext').text().trim();
         $(this).attr('id', 'item-' + idText);
@@ -37,51 +147,7 @@ $(document).ready(function() {
             document.body.style.touchAction = "";
         }
     }
-    
-    // TRIO ANIMATION
-    gsap.to(".team-image-wrapper", {
-        scrollTrigger: {
-            trigger: ".image-trio",
-            start: "top top",
-            end: "+=1000",
-            pin: ".image-trio",
-            scrub: true,
-        },
-        height: 0,
-        stagger: 0.1,
-    });
-
-    gsap.from(".team-trio-text-wrapper", {
-        scrollTrigger: {
-            trigger: ".image-trio",
-            start: "top top",
-            end: "+=1000",
-            scrub: true,
-        },
-        yPercent: 5,
-        stagger: 0.1,
-        delay: 0.2,
-    });
-
-    // TEXT ANIMATION
-    text = document.querySelector(".text-xl-wrapper");
-    textWidth = text.getBoundingClientRect().width;
-
-    textTravel = window.innerWidth - textWidth;
-
-    gsap.to(".text-xl-wrapper", {
-        scrollTrigger: {
-            trigger: ".text-xl",
-            start: "top top",
-            end: "+=1500",
-            scrub: true,
-            pin: true,
-        },
-        x: textTravel,
-    });
-
-    // TEAM ANIMATION
-    matchM.add("(min-width: 992px)", () => {
+    mm.add("(min-width: 992px)", () => {
         // Select hover triggers
         const teamNames = gsap.utils.toArray(".team-col-item");
 
@@ -95,17 +161,18 @@ $(document).ready(function() {
             const imageLarge = relatedBio.querySelector(".team-image-large");
             const title = relatedBio.querySelector(".title-wrapper");
             const bio = relatedBio.querySelector(".bio-text-wrapper");
-            const links = relatedBio.querySelector(".bio-links");
+            const email = relatedBio.querySelector(".email-wrapper");
+            const close = relatedBio.querySelector(".close-wrapper");
 
             function closeBio() {
                 let closeTL = gsap.timeline();
 
                 closeTL
-                .to([imageLarge, title, bio, links], { opacity: 0, duration: 0.3 })
+                .to([imageLarge, title, bio, email, close], { opacity: 0, duration: 0.3 })
                 .to(".bio-col-wrapper", { opacity: 0, backgroundColor: "rgba(255, 255, 255, 0.0)", duration: 0.3 }, "<")
                 .set(relatedBio, { display: "none" })
                 .set(".bio-col-wrapper", { display: "none" })
-                .set([imageLarge, title, bio, links], { opacity: 1 })
+                .set([imageLarge, title, bio, email, close], { opacity: 1 })
                 .set(".bio-col-wrapper", { opacity: 1, backgroundColor: "rgba(255, 255, 255, 0.3)" });
 
                 activeId = null;
@@ -137,47 +204,48 @@ $(document).ready(function() {
                 .set(relatedBio, { display: "flex" }, "<")
                 .set(".bio-col-wrapper", { display: "flex" })
                 .from(".bio-col-wrapper", { opacity: 0, backgroundColor: "rgba(255, 255, 255, 0.0)", duration: 0.3 })
-                .from([imageLarge, title, bio, links], { opacity: 0, duration: 0.3 }, "<");
+                .from([imageLarge, title, bio, email, close], { opacity: 0, duration: 0.3 }, "<");
 
                 freezeScroll();
             });
         });
     });
 
-
     // WE'RE GOOD AT ANIMATION
     window.addEventListener("load", () => {
         document.fonts.ready.then(() => {
-            setTimeout(() => {
-                const split = SplitText.create(".split-me", {
-                    type: "words",
-                    autoSplit: true,
-                });
+            SplitText.create(".split-me", {
+                type: "words",
+                autoSplit: true,
+                onSplit: (self) => {
+                    return gsap.from(self.words, {
+                        opacity: 0,
+                        stagger: 1,
+                        scrollTrigger: {
+                            trigger: ".our-skills",
+                            start: "top top",
+                            end: "+=1500",
+                            scrub: true,
+                            pin: true,
+                            ease: "none",
+                        }
+                    });
+                }
+            });
+        });
+    });
 
-                gsap.from(split.words, {
-                    scrollTrigger: {
-                        trigger: ".our-skills",
-                        start: "top top",
-                        end: "+=1500",
-                        scrub: true,
-                        pin: true,
-                    },
-                    opacity: 0,
-                    stagger: 1,
-                });
-
-                gsap.from(".skills-icon", {
-                    scrollTrigger: {
-                        trigger: ".our-skills",
-                        start: "top top",
-                        end: "+=1500",
-                        scrub: true,
-                    },
-                    opacity: 0,
-                    stagger: 1,
-                });
-                
-            }, 100);
+    mm.add("(min-width: 479px)", () => {
+        gsap.from(".skills-icon", {
+            opacity: 0,
+            stagger: 1,
+            scrollTrigger: {
+                trigger: ".our-skills",
+                start: "top top",
+                end: "+=1500",
+                scrub: true,
+                ease: "none",
+            }
         });
     });
 
@@ -186,8 +254,9 @@ $(document).ready(function() {
         scrollTrigger: {
             trigger: ".footer",
             start: "top 50%",
-            end: "top top",
+            end: "bottom bottom",
             scrub: true,
+            ease: "none",
         }
     });
 
@@ -212,6 +281,7 @@ $(document).ready(function() {
                 start: "top 50%",
                 end: "bottom bottom",
                 scrub: true,
+                ease: "none",
                 onUpdate: function (self) {
                     const progress = self.progress;
                     if (window.footerLottie) {
